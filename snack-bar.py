ecea4640547655f505e8b022d0a781f377fbf08d
Beto Drumond <humbertowdrumond@gmail.com>
2020-10-24 17:07:53 -0300

    0.3.2v

--- snack-bar.py
+++ snack-bar.py
@@ -1,5 +1,5 @@
 def clear():
-    print('\n'*40)
+    print('\n' * 400)
 
 
 def login():
@@ -9,11 +9,12 @@ def login():
     print('\tHELLO OPERATOR! INSERT YOUR CREDENTIALS:\n')
     # user: admin
     user = input('\tUSER:          ')
-    # senha: admin
+    # password: admin
     password = input('\tPASSWORD:      ')
 
     if user in users:
         if password == passwords[passwords.index(user)]:
+            clear()
             menu()
         else:
             clear()
@@ -27,7 +28,47 @@ def login():
 
 def menu():
     print('\tCHOOSE AN OPTION:')
+    print('\t1 - NEW BUY                2 - REGISTER PRODUCT')
+    print('\t3 - MODIFY A PRODUCT       4 - SALES REPORT')
+    print('\t                           0 - LOGOFF')
+    option = input('\t')
+
+    if option == '1':
+        new_buy()
+
+    elif option == '2':
+        register_product()
+
+    elif option == '3':
+        modify_product()
+
+    elif option == '4':
+        sales_report()
+
+    elif option == '0':
+        clear()
+        login()
+
+    else:
+        print('\tINVALID OPTION, TRY AGAIN:')
+        menu()
 
 
+def new_buy():
+    pass
 
-login()
+
+def register_product():
+    pass
+
+
+def modify_product():
+    pass
+
+
+def sales_report():
+    pass
+
+
+# login()
+menu()
